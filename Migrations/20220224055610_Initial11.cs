293bb0c948c1f4589479c4252832c46b39860361
khalil <khilel208@gmail.com>
2022-02-24 09:18:02 +0300

    Initial commit

---
+++ Migrations/20220224055610_Initial11.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTAuthentication.NET6._0.Migrations
+{
+    public partial class Initial11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
